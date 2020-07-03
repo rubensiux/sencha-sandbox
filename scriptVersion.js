add3a4c687c3e5faa66e583b7ce011fb654a5b90
RSSOSASO <ruben.sosa@gnp.com.mx>
2020-07-03 17:12:32 -0500

    chore(release): 9.9.39

--- scriptVersion.js
+++ scriptVersion.js
@@ -1,11 +1,19 @@
 const updateJsonFile = require('update-json-file');
+const shell = require('shelljs');
 
 const packageApp = require('./package.json');
 
 const versionApp = packageApp.version;
 const filePath = './app.json';
 
-updateJsonFile(filePath, function(data) {
+updateJsonFile(filePath, (data) => {
     data.version = versionApp;
     return data
-})
+}).then(() => {
+    if (!shell.which('git')) {
+        shell.echo('Sorry, this script requires git');
+        shell.exit(1);
+    } else {
+        shell.exec('git add ./app.json');
+    }
+});
